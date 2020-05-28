e3437106c3f318cabe7c5a0f9d845d07ee745767
litongar <garyliton@hotmail.com>
2020-05-28 17:37:36 -0400

    move_zeroes

---
+++ move_zeroes.py
@@ -0,0 +1,11 @@
+def moveZeroes(nums):
+    count = 0
+    for num in nums:
+        if num != 0:
+            nums[count] = num
+            count += 1
+
+    while count < len(nums):
+        nums[count] = 0
+        count += 1
+    return nums
