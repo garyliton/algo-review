32db7676e24f414b5c2a9a696e8607f08e13a572
litongar <garyliton@hotmail.com>
2020-05-28 17:20:27 -0400

    invert_binary_tree

---
+++ invert_binary_tree.py
@@ -0,0 +1,10 @@
+def invertTree(root):
+    if not root:
+        return
+
+    left = self.invertTree(root.left)
+    right = self.invertTree(root.right)
+    root.left = right
+    root.right = left
+
+    return root
